f3c68fbdced0045239c0b65a0749832c830d3cc0
Josh Levinger <josh@levinger.net>
2015-07-13 12:05:47 -0700

    redirect js needs to be inline

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,14 +37,6 @@
 					skel.breakpoint('narrower').active
 				);
 			});
-
-		// Redirect to main domain if we're being loaded on gh-pages project url
-		if (window.location.host !== 'callpower.org') {
-			page = window.location.pathname.replace('/call-power-org/','');
-			redirect = 'http://www.callpower.org/'+page;
-			window.location.replace(redirect);
-		}
-
 	});
 
 })(jQuery);
