c10c2f09ffa53a2d8041b58329f683fc24473e83
Josh Levinger <josh@levinger.net>
2017-05-15 16:31:13 -0700

    political_data.lookup only valid when campaign.segment_by == SEGMENT_BY_LOCATION

--- call_server/political_data/lookup.py
+++ call_server/political_data/lookup.py
@@ -1,13 +1,20 @@
-from ..extensions import cache
-from ..campaign.constants import INCLUDE_CUSTOM_FIRST, INCLUDE_CUSTOM_LAST, INCLUDE_CUSTOM_ONLY
-
+from flask import current_app
 from collections import OrderedDict
+
+from ..extensions import cache
+from ..campaign.constants import INCLUDE_CUSTOM_FIRST, INCLUDE_CUSTOM_LAST, INCLUDE_CUSTOM_ONLY, SEGMENT_BY_LOCATION
+
 
 def locate_targets(location, campaign, cache=cache):
     """
     Convenience method to get targets for location in a given campaign.
+    Assumes campaign.segment_by == SEGMENT_BY_LOCATION
     @return  list of target uids
     """
+
+    if campaign.segment_by and campaign.segment_by is not SEGMENT_BY_LOCATION:
+        current_app.logger.error('Called locate_targets on campaign where segment_by=%s (%s)' % (campaign.segment_by, campaign.id))
+        return []
 
     campaign_data = campaign.get_campaign_data(cache)
     location_targets = campaign_data.get_targets_for_campaign(location, campaign)
