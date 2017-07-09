c4a31bdd23fabbe2a778b5297594f012f8e33d96
Josh Levinger <josh@levinger.net>
2017-07-09 14:01:13 -0700

    Bump version to 1.3.2

--- call_server/config.py
+++ call_server/config.py
@@ -8,7 +8,7 @@ class DefaultConfig(object):
     DEBUG = False
     TESTING = False
     ENVIRONMENT = "Default"
-    VERSION = "1.3.1"
+    VERSION = "1.3.2"
 
     APP_NAME = "call_server"
     APPLICATION_ROOT = None  # the path where the application is configured
