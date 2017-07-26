79263ea00e67fa2a04d65d86bc20b77d612e4021
Josh Levinger <josh@levinger.net>
2017-07-26 11:27:14 -0700

    adjust twilio connect-src

--- call_server/extensions.py
+++ call_server/extensions.py
@@ -37,7 +37,7 @@ CALLPOWER_CSP = {
     'style-src': ['\'self\'', '\'unsafe-inline\'', 'fonts.googleapis.com'], 
     'font-src': ['\'self\'', 'fonts.gstatic.com'],
     'media-src': ['\'self\'', 'media.twiliocdn.com'],
-    'connect-src': ['*.twilio.com', ]
+    'connect-src': ['\'self\'', 'wss://*.twilio.com', ]
 }
 # unsafe-inline needed to render <script> tags without nonce
 # unsafe-eval needed to run bootstrap templates
