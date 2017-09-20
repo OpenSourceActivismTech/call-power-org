59b381b017eb0908e3176addadc0b8b878814492
Josh Levinger <josh@levinger.net>
2017-09-20 14:35:27 -0700

    Bump version 1.3.8

--- call_server/config.py
+++ call_server/config.py
@@ -7,7 +7,7 @@ class DefaultConfig(object):
     PROJECT = 'CallPower'
     DEBUG = False
     TESTING = False
-    VERSION = "1.3.7"
+    VERSION = "1.3.8"
     ENVIRONMENT = os.environ.get('APP_ENVIRONMENT', "Default")
 
     APP_NAME = "call_server"
